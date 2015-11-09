8abe1fe4cc5b2e9822e1e5d2388e98634f506c56
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-11-09 15:44:42 -0800

    sms.js changed to use demo settings

--- whatsgood-m/server/sms.js
+++ whatsgood-m/server/sms.js
@@ -1,4 +1,11 @@
-if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+// Make sure to use real twilio account
+// SMS.twilio = {
+//	ACCOUNT_SID: Meteor.settings.TWILIO.SID,
+// AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
+// };
+if (Meteor.isServer) {
+	if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
 	Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
   	Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+	}
 }
