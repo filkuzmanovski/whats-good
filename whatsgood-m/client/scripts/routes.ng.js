458713867eb22148d9931ceef30276fbd1fa0f0f
Filip Kuzmanovski <filkuzmanovski@gmail.com>
2015-11-10 12:41:13 -0800

    added login view

--- whatsgood-m/client/scripts/routes.ng.js
+++ whatsgood-m/client/scripts/routes.ng.js
@@ -1,32 +1,47 @@
 angular
-	.module('WhatsGood')
-	.config(config);
+  .module('WhatsGood')
+  .config(config);
 
 function config($stateProvider, $urlRouterProvider) {
-	$stateProvider
-	.state('tab', {
-		url: '/tab',
-		abstract: true,
-		templateUrl: 'client/templates/tabs.ng.html'
-	})
-	.state('tab.chats', {
-		url: '/chats',
-		views: {
-			'tab-chats': {
-				templateUrl: 'client/templates/chats.ng.html',
-				controller: 'ChatsCtrl'
-			}
-		}
-	})
-	.state('tab.chat-detail', {
-		url: '/chats/:chatId',
-		views: {
-			'tab-chats': {
-				templateUrl: 'client/templates/chat-detail.ng.html',
-				controller: 'ChatDetailCtrl'
-			}
-		}
-	});
+  $stateProvider
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'client/templates/tabs.ng.html'
+    })
+    .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'client/templates/chats.ng.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'client/templates/chat-detail.ng.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'client/templates/login.ng.html',
+      controller: 'LoginCtrl'
+    })
+    .state('confirmation', {
+      url: '/confirmation/:phone',
+      templateUrl: 'client/templates/confirmation.ng.html',
+      controller: 'ConfirmationCtrl'
+    })
+    .state('profile', {
+      url: '/profile',
+      templateUrl: 'client/templates/profile.ng.html',
+      controller: 'ProfileCtrl'
+    });
 
-	$urlRouterProvider.otherwise('tab/chats');
+  $urlRouterProvider.otherwise('tab/chats');
 }
